ebdc254daa1b40ebecf23c42b66f6bcc6ccb8943
Daniel Chunn <danielchunn@gmail.com>
2017-09-29 17:39:07 -0400

    fixed some regex issues
    fixed extra characters being added onto csv file, csv file now takes in
    clean tweets

--- tweetMood.py
+++ tweetMood.py
@@ -18,18 +18,19 @@ api = tweepy.API(auth)
 # Step 2 - Prepare search
 hash_tag = '#divinityoriginalsin2'
 since_date = "2017-09-20"
-until_date = "2017-09-27"
+until_date = "2017-09-29"
 number_of_tweets = 100
 
 public_tweets = api.search(hash_tag, count = number_of_tweets, since = since_date, until = until_date)
 
-
-# Step 3 - Write to CSV
+# Step 3 - write to CSV
 def clean_tweet(tweet):
+
+	#https://stackoverflow.com/questions/8376691/how-to-remove-hashtag-user-link-of-a-tweet-using-regular-expression/8377440
 	return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
 def get_tweet_sentiment(tweet):
-    analysis = TextBlob(clean_tweet(tweet))
+    analysis = TextBlob(tweet)
     if analysis.sentiment.polarity > 0:
         return 'positive'
     elif analysis.sentiment.polarity == 0:
@@ -37,10 +38,10 @@ def get_tweet_sentiment(tweet):
     else:
     	return 'negative'
 
-with open('%s_tweets.csv' % hash_tag, 'w') as csvfile:
+with open('%s_tweets.csv' % hash_tag, 'w', encoding='utf8') as csvfile:
 	writer = csv.writer(csvfile, delimiter=",")
 	for tweet in public_tweets:
-		analysis = TextBlob(tweet.text)
-		sentiment = get_tweet_sentiment(tweet.text)
-		arr = [tweet.text.encode('utf8'), sentiment]
+		c_tweet = clean_tweet(tweet.text)
+		sentiment = get_tweet_sentiment(c_tweet)
+		arr = [c_tweet, sentiment]
 		writer.writerow(arr)
